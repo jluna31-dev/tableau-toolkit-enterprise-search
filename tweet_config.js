68a59f456585f199dd3d8feed62d9ea3af5334e7
Juan Luna <jluna@twitter.com>
2022-09-30 12:45:26 -0400

    Fix tweet_config.js again

--- tweet_config.js
+++ tweet_config.js
@@ -2,7 +2,7 @@ var tweet_config = {};
 
 tweet_config.queryString = "$BTC OR $ETH OR $DOGE OR $ADA";
 tweet_config.fromDate = "202207010000";
-tweet_config.toDate= "202208010000a";
+tweet_config.toDate= "202208010000";
 tweet_config.maxResults = 500;
 tweet_config.returnOnly = 10000;
 
